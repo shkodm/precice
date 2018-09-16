4dc4ba762fe016e822fd4c37f7d9a4db45cf3cbd
shkodm <dmytro.sashko@tum.de>
2018-09-16 16:45:20 +0200

    Reboot cache

--- tools/travis-install-dependencies.sh
+++ tools/travis-install-dependencies.sh
@@ -9,12 +9,6 @@ set -e
 LOCAL_INSTALL=$1
 
 
-# Download, compile and install Boost
-wget -nv 'http://downloads.sourceforge.net/project/boost/boost/1.65.0/boost_1_65_0.tar.bz2' -O - | tar xj
-cd boost_1_65_0
-./bootstrap.sh --prefix=$LOCAL_INSTALL > ~/boost.bootstrap
-./b2 -j2 --with-program_options --with-test --with-filesystem --with-log install > ~/boost.b2
-
 # Don't test for $LOCAL_INSTALL, because it's created by the cacher.
 if [ ! -d $LOCAL_INSTALL/include ]; then
     mkdir -p $LOCAL_INSTALL/include $LOCAL_INSTALL/lib
@@ -22,9 +16,8 @@ if [ ! -d $LOCAL_INSTALL/include ]; then
     # Download and extract Eigen
     wget -nv http://bitbucket.org/eigen/eigen/get/3.3.2.tar.bz2 -O - | tar xj -C $LOCAL_INSTALL/include --strip-components=1 eigen-eigen-da9b4e14c255/Eigen
 
-    # Download, compile and install Boost
-    wget -nv 'http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.bz2' -O - | tar xj
-    cd boost_1_60_0
+    wget -nv 'https://dl.bintray.com/boostorg/release/1.65.0/source/boost_1_65_0.tar.bz2' -O - | tar xj
+    cd boost_1_65_0
     ./bootstrap.sh --prefix=$LOCAL_INSTALL > ~/boost.bootstrap
     ./b2 -j2 --with-program_options --with-test --with-filesystem --with-log install > ~/boost.b2
 
