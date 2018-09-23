a7127b37180e449d97b3e2dd3ba58e75f1033398
shkodm <dmytro.sashko@tum.de>
2018-09-23 21:15:30 +0200

    Tryying to fix problem with linking with boost

--- tools/travis-install-dependencies.sh
+++ tools/travis-install-dependencies.sh
@@ -8,7 +8,6 @@ set -e
 
 LOCAL_INSTALL=$1
 
-
 # Don't test for $LOCAL_INSTALL, because it's created by the cacher.
 if [ ! -d $LOCAL_INSTALL/include ]; then
     mkdir -p $LOCAL_INSTALL/include $LOCAL_INSTALL/lib
@@ -21,6 +20,10 @@ if [ ! -d $LOCAL_INSTALL/include ]; then
     cd boost_1_60_0
     ./bootstrap.sh --prefix=$LOCAL_INSTALL > ~/boost.bootstrap
     ./b2 -j2 --with-program_options --with-test --with-filesystem --with-log install > ~/boost.b2
+    # update dynamic linker runtime binding on mac
+    if [ $TRAVIS_OS_NAME = osx ]; then
+      sudo update_dyld_shared_cache
+    fi
 
     # Download and compile PETSc
     cd $LOCAL_INSTALL
@@ -42,7 +45,6 @@ if [ $TRAVIS_OS_NAME = osx ]; then
 else
 	CMAKE_URL="http://www.cmake.org/files/v3.10/cmake-3.10.1-Linux-x86_64.tar.gz"
 fi 
-
 mkdir -p ${LOCAL_INSTALL}/cmake
 wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C ${LOCAL_INSTALL}/cmake
 # make cmake available
