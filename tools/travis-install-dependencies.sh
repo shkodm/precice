c4839bd264cead7db10c3d6be523244989240011
shkodm <dmytro.sashko@tum.de>
2018-09-23 11:09:36 +0200

    Adding installation of eigen using cmake

--- tools/travis-install-dependencies.sh
+++ tools/travis-install-dependencies.sh
@@ -8,13 +8,24 @@ set -e
 
 LOCAL_INSTALL=$1
 
+# get version of cmake, that works with boost 1.60.0
+#if [ ! -d $LOCAL_INSTALL/cmake ]; then
+rm -rf ${LOCAL_INSTALL}/cmake
+CMAKE_URL="http://www.cmake.org/files/v3.10/cmake-3.10.1-Linux-x86_64.tar.gz"
+mkdir -p ${LOCAL_INSTALL}/cmake
+wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C ${LOCAL_INSTALL}/cmake
+# make cmake available
+export PATH=${LOCAL_INSTALL}/cmake/bin:${PATH}
 
 # Don't test for $LOCAL_INSTALL, because it's created by the cacher.
 if [ ! -d $LOCAL_INSTALL/include ]; then
     mkdir -p $LOCAL_INSTALL/include $LOCAL_INSTALL/lib
 
-    # Download and extract Eigen
-    wget -nv http://bitbucket.org/eigen/eigen/get/3.3.2.tar.bz2 -O - | tar xj -C $LOCAL_INSTALL/include --strip-components=1 eigen-eigen-da9b4e14c255/Eigen
+    # Download and extract Eigen with cmake
+    wget -nv http://bitbucket.org/eigen/eigen/get/3.3.2.tar.bz2 -O - | tar xj -C
+    cd eigen-eigen-da9b4e14c255; make build
+    cmake -DCMAKE_INSTALL_PREFIX=$LOCAL_INSTALL
+    make install
 
     wget -nv 'https://dl.bintray.com/boostorg/release/1.65.0/source/boost_1_65_0.tar.bz2' -O - | tar xj
     cd boost_1_65_0
@@ -30,9 +41,3 @@ if [ ! -d $LOCAL_INSTALL/include ]; then
     make > ~/petsc.make
 fi
 
-# get version of cmake, that works with boost 1.60.0 
-#if [ ! -d $LOCAL_INSTALL/cmake ]; then
-rm -rf ${LOCAL_INSTALL}/cmake 
-CMAKE_URL="http://www.cmake.org/files/v3.10/cmake-3.10.1-Linux-x86_64.tar.gz"
-mkdir -p ${LOCAL_INSTALL}/cmake
-wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C ${LOCAL_INSTALL}/cmake
