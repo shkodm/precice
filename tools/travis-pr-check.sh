08f31806a8479e3ecc52a9421ec3d2616f1ce445
shkodm <dmytro.sashko@tum.de>
2018-10-21 13:08:25 +0200

    Move everything to the pr subshell [ci skip]

--- tools/travis-pr-check.sh
+++ tools/travis-pr-check.sh
@@ -4,14 +4,23 @@
 
 set -x
 
-# Chechking change log
+# generic bot message start
+BOT_MSG="Thank you for your contribution"
+
+
+# Checking change log
 # we merform git diff between commit ids of current branch and the one from PR
+changelog_missing=0
 lines_changed=$(git log | sed -n '2p' | awk '{print $2, $3}' | xargs git diff --numstat | awk '{ sum+= $1 + $2 ; } END { print sum; }')
 if [ $lines_changed -gt 100 ]; then
   git log | sed -n '2p' | awk '{print $2, $3}' | xargs git diff --numstat | grep 'CHANGELOG.md'
   if [ "$?" -eq 1 ]; then
+    changelog_missing=1
     BOT_MSG+="It seems, like you  forgot to update CHANGELOG.md"
-    echo "It seems like you forgot to update CHANGELOG.md"
-    exit 1
   fi
+fi
+
+if [[ "$changelog_missing" -eq 1 ]]; then
+  curl -s -H "Authorization: token $TRAVIS_ACCESS_TOKEN" -X POST -d "{\"body\": \"$BOT_MSG\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+  exit 1
 fi
