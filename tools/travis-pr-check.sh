6ae9f0fdb60f764d8d1e368406dd9dcb1027491c
shkodm <dmytro.sashko@tum.de>
2018-10-21 13:30:58 +0200

    Update message styling [ci skip]

--- tools/travis-pr-check.sh
+++ tools/travis-pr-check.sh
@@ -5,22 +5,23 @@
 set -x
 
 # generic bot message start
-BOT_MSG="Thank you for your contribution"
+BOT_MSG="Thank you for your contribution.\n"
 
 
 # Checking change log
 # we merform git diff between commit ids of current branch and the one from PR
-changelog_missing=0
+pr_invalid=0
 lines_changed=$(git log | sed -n '2p' | awk '{print $2, $3}' | xargs git diff --numstat | awk '{ sum+= $1 + $2 ; } END { print sum; }')
 if [ $lines_changed -gt 100 ]; then
   git log | sed -n '2p' | awk '{print $2, $3}' | xargs git diff --numstat | grep 'CHANGELOG.md'
   if [ "$?" -eq 1 ]; then
-    changelog_missing=1
-    BOT_MSG+="It seems, like you  forgot to update CHANGELOG.md"
+    pr_invalid=1
+    BOT_MSG+="\nSome suggestions for your pull request\n* It seems, like you  forgot to update CHANGELOG.md"
   fi
 fi
 
-if [[ "$changelog_missing" -eq 1 ]]; then
+
+if [[ "$pr_invalid" -eq 1 ]]; then
   curl -s -H "Authorization: token $TRAVIS_ACCESS_TOKEN" -X POST -d "{\"body\": \"$BOT_MSG\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
   exit 1
 fi
