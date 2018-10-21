89c3ad57fa4763e744d461934e3f5707be705220
shkodm <dmytro.sashko@tum.de>
2018-10-21 12:26:42 +0200

    Switch to return codes and and debug[ci skip]

--- tools/travis-pr-check.sh
+++ tools/travis-pr-check.sh
@@ -10,9 +10,10 @@ set -x
 # we merform git diff between commit ids of current branch and the one from PR
 lines_changed=$(git log | sed -n '2p' | awk '{print $2, $3}' | xargs git diff --numstat | awk '{ sum+= $1 + $2 ; } END { print sum; }')
 if [ $lines_changed -gt 100 ]; then
-  a=$( git log | sed -n '2p' | awk '{print $2, $3}' | xargs git diff --numstat | grep 'CHANGELOG.md')
-  if [ -z "$a" ]; then
-    BOT_MSG+="It seems, like you are forgot to update CHANGELOG.md"
+  git log | sed -n '2p' | awk '{print $2, $3}' | xargs git diff --numstat | grep 'CHANGELOG.md'
+  if [ "$?" -eq 1 ]; then
+    BOT_MSG+="It seems, like you  forgot to update CHANGELOG.md"
+    echo "It seems like you forgot to update CHANGELOG.md"
     exit 1
   fi
 fi
